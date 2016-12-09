cd98c3c5cf5aa073ce9a64c1292902c4c90032b6
Goran Marjanovic <gormar108@gmail.com>
2016-12-09 10:47:07 +0100

    radi zbrajanje i množenje

--- kalkulator.rb
+++ kalkulator.rb
@@ -1,3 +1,17 @@
+
 puts "Welcome to calculator"
 
-puts ARGV[0].to_i+ ARGV[1].to_i
+puts ARGV.inspect
+
+if ARGV[1] == "*"
+  puts "Radimo mnozenje" 
+  puts ARGV[0].to_i * ARGV[2].to_i
+end
+
+if ARGV[1] == "+"
+  puts "Radimo zbrajanje" 
+  puts ARGV[0].to_i + ARGV[2].to_i
+end
+
+
+ 
